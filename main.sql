cbe1eb34b8d007f4d8ba79fdb53b5e027f26cdd8
John Belanger <Belangerremodeling@gmail.com>
2022-03-09 18:59:25 +0000

    Initial commit

---
+++ main.sql
@@ -0,0 +1,10 @@
+.open dbdogs
+
+DROP TABLE IF EXISTS dogs;
+
+CREATE TABLE IF NOT EXISTS dogs(
+  dogid INTEGER NOT NULL PRIMARY KEY,
+  dogname VARCHAR(20) NOT NULL,
+  breed VARCHAR(20),
+  color VARCHAR(50)
+);
